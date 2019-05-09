c0b5e11f2f2ffc5bf1aaa3f629b0866ab592cae3
codegymdanang <codegymdanang@gmail.com>
2019-05-09 16:00:50 +0700

    Run on Tomcat server

--- src/main/java/codegym/danang/DemoApplication.java
+++ src/main/java/codegym/danang/DemoApplication.java
@@ -2,11 +2,13 @@ package codegym.danang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
