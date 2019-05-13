aef73c2e859bf2750a0c7fc13b1a98e946cb7938
codegymdanang <codegymdanang@gmail.com>
2019-05-13 17:23:29 +0700

    Themeleaf

--- src/main/java/codegym/danang/controller/SimpleController.java
+++ src/main/java/codegym/danang/controller/SimpleController.java
@@ -3,6 +3,7 @@ package codegym.danang.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +15,11 @@ public class SimpleController {
 	@RequestMapping("/")
     public String homePage(Model model) {
         model.addAttribute("appName", appName);
-        return "home";
+        return "p2";
+    }
+
+    @GetMapping("/page1")
+    public String page2() {
+        return "p1";
     }
 }
