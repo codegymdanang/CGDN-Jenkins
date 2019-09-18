28d31fb7b767133f650aafa2bbb1bd658faf9bc5
codegymdanang <codegymdanang@gmail.com>
2019-09-18 20:05:23 +0700

    init

--- src/main/java/codegym/danang/DemoApplication.java
+++ src/main/java/codegym/danang/DemoApplication.java
@@ -3,11 +3,11 @@ package codegym.danang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("codegym.danang.repo")
-@EntityScan("codegym.danang.entity")
+
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
 
@@ -16,7 +16,14 @@ public class DemoApplication extends SpringBootServletInitializer {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
 
 
 }
+
+
+
 
